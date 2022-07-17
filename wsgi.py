bf3bf3fbfb6face19e575bfa469b57aec44b95ae
Circus Records <LabelAssistant@JK-2.broadband>
2022-07-17 17:04:41 +0100

    Adding WSGI

---
+++ wsgi.py
@@ -0,0 +1,8 @@
+import os
+from django.core.wsgi import get_wsgi_application
+
+server_env = 'production'
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'practice_site_1.settings.production')
+
+application = get_wsgi_application()
