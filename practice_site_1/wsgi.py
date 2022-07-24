02ba02c891611d21461ba5f1991d9ec538798fad
Sonny Baker <sonny@hactar.is>
2022-07-24 11:02:27 +0100

    Unify wsgi files

--- practice_site_1/wsgi.py
+++ practice_site_1/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "practice_site_1.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f'practice_site_1.settings.production')
 
 application = get_wsgi_application()
--- wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-server_env = 'production'
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'practice_site_1.settings.production')
-
-application = get_wsgi_application()
