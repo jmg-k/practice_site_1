25efe4e3a862958539e54db38d2121a94345bc3c
Sonny Baker <sonny@hactar.is>
2022-07-24 10:47:52 +0100

    Production settings

--- practice_site_1/settings/production.py
+++ practice_site_1/settings/production.py
@@ -6,15 +6,23 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['circus-records.co.uk', 'www.circus-records.co.uk']
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-
+CORS_ORIGIN_ALLOW_ALL = True
+CSRF_TRUSTED_ORIGINS = ['https://circus-records.co.uk', ]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DB_HOST'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': 'localhost',
-        'PORT': 5432
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.environ['DB_HOST'],
+#         'USER': os.environ['DB_USER'],
+#         'PASSWORD': os.environ['DB_PASSWORD'],
+#         'HOST': 'localhost',
+#         'PORT': 5432
+#     }
+# }
